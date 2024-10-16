b4ec921f88eaea4f70b9ec10c50b8bc6a58e89ce
Vladimir Tinica <vltinica@gmail.com>
2024-10-16 05:14:57 -0400

    file css

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,15 @@
 import React from "react";
+import "./Post.css"
 
-const Post = () => {
+const Post = (props) => {
+  const {id, title, userId, body} = props
+
   return (
-    <div>
-      <h1>This is post</h1>
+    <div className="post">
+      <small>{id}</small>
+      <h2>{title}</h2>
+      <p>{body}</p>
+      <h3>User ID: {userId}</h3>
     </div>
   );
 };
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,22 +3,28 @@ import { useState, useEffect } from "react";
 import Post from "./Post";
 
 const Posts = () => {
-  const [post, setPost] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState("")
 
   const url = "https://jsonplaceholder.typicode.com/posts";
 
   useEffect(() => {
     fetch(url)
       .then((response) => response.json())
-      .then((json) => {
-        console.log(json)
-        setPost(json)
-      })
-      .catch((error) => console.log(error.message))
+      .then((posts) => setPosts(posts))
+      .catch((error) => setError(error.message));
   }, []);
+
+  if(error) {
+    return (
+      <h1>Error: {error}</h1>
+    )
+  }
   return (
     <div>
-      <Post />
+      {posts.map((post) => (
+        <Post key={post.id} {...post} />
+      ))}
       <Post />
     </div>
   );
