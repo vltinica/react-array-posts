3b05705ad5a7ba1acb4ff87bdf4d696bcdf2b12e
Vladimir Tinica <vltinica@gmail.com>
2024-10-18 06:21:40 -0400

    finally

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -4,28 +4,37 @@ import Post from "./Post";
 
 const Posts = () => {
   const [posts, setPosts] = useState([]);
-  const [error, setError] = useState("")
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   const url = "https://jsonplaceholder.typicode.com/posts";
 
   useEffect(() => {
     fetch(url)
       .then((response) => response.json())
-      .then((posts) => setPosts(posts))
-      .catch((error) => setError(error.message));
+      .then((data) => setPosts(data))
+      .catch((error) => setError(error.message))
+      .finally(() => setIsLoading(false));
   }, []);
 
-  if(error) {
-    return (
-      <h1>Error: {error}</h1>
-    )
+  if (error) {
+    return <h1>Error: {error}</h1>;
   }
   return (
     <div>
-      {posts.map((post) => (
-        <Post key={post.id} {...post} />
-      ))}
-      <Post />
+      {isLoading ? (
+        <div className="loadingStyle">
+          <h1>Loading...</h1>
+        </div>
+      ) : (
+        <div>
+          <h1>Posts</h1>
+          <hr />
+          {posts.map((post) => (
+            <Post key={post.id} {...post} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
